e63a770d0866bdba854f092ed32b36de1dc77ac8
Saurlax <saurlax@qq.com>
2024-10-22 21:55:03 +0800

    feat: csi display via mqtt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  modules: ["@vant/nuxt"],
+  modules: ["@vant/nuxt", "nuxt-echarts"],
   app: {
     head: {
       title: "微智护",
+    },
+  },
+  echarts: {
+    charts: ["HeatmapChart"],
+    components: [
+      "LegendComponent",
+      "TooltipComponent",
+      "GridComponent",
+      "VisualMapComponent",
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      mqttBrokerUri: "",
     },
   },
 });
--- pages/live.vue
+++ pages/live.vue
@@ -1,79 +1,91 @@
 <script setup lang="ts">
-import { onUnmounted, reactive, ref } from 'vue'
+import mqtt from 'mqtt';
+import { ref } from 'vue';
+import Papa from 'papaparse';
 
-import VCharts from 'vue-echarts'
-import { use } from 'echarts/core'
-import { CanvasRenderer } from 'echarts/renderers'
-import { LegendComponent, TooltipComponent, GridComponent, VisualMapComponent, } from 'echarts/components'
-import { HeatmapChart } from 'echarts/charts'
+const runtimeConfig = useRuntimeConfig()
 
-use([HeatmapChart, CanvasRenderer, LegendComponent, TooltipComponent, GridComponent, VisualMapComponent])
+onMounted(() => {
+  const client = mqtt.connect(runtimeConfig.public.mqttBrokerUri, {
+    username: 'username',
+    password: 'password'
+  })
+  client.on('connect', () => {
+    console.log(`Connected to ${runtimeConfig.public.mqttBrokerUri}`);
+    client.subscribe('wiguard/csi')
+  })
 
+  client.on('message', (topic, message) => {
+    const { data } = Papa.parse(message.toString(), {
+      dynamicTyping: true,
+      skipEmptyLines: true
+    })
+
+    data.forEach((row: any, index) => {
+      const csi: number[] = JSON.parse(row[24])
+      for (let i = 0; i < csi.length; i += 2) {
+        csidata.value.push([index, i / 2, Math.sqrt(csi[i] ** 2 + csi[i + 1] ** 2)])
+      }
+    })
+
+    console.log(csidata.value);
+
+  })
+})
 
 const subcarrier = Array.from({ length: 30 }, (_, i) => i)
-const time = Array.from({ length: 301 }, (_, i) => {
-  return (i - 300) / 10 + 's'
+const time = Array.from({ length: 300 }, (_, i) => {
+  return i
 })
 
-const data: any = []
+const csidata = ref<number[][]>([])
 
-const option = reactive({
-  grid: {
-    top: 20,
-    left: 30,
-    right: 30,
-    bottom: 20
-  },
-  xAxis: {
-    type: 'category',
-    data: time,
-    axisLabel: {
-      interval: 49
+
+const option = computed<ECOption>(() => {
+  return {
+    grid: {
+      top: 20,
+      left: 30,
+      right: 30,
+      bottom: 20
     },
-    axisPointer: {
-      show: true
+    xAxis: {
+      type: 'category',
+      data: time,
+      axisLabel: {
+        interval: 49
+      },
+      axisPointer: {
+        show: true
+      }
+    },
+    yAxis: {
+      type: 'category',
+      data: subcarrier,
+      axisPointer: {
+        show: true
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 35,
+      right: 5,
+      top: 'middle',
+      itemWidth: 10,
+      inRange: {
+        colorHue: [240, 0]
+      }
+    },
+    series: {
+      type: 'heatmap',
+      data: csidata.value,
     }
-  },
-  yAxis: {
-    type: 'category',
-    data: subcarrier,
-    axisPointer: {
-      show: true
-    }
-  },
-  visualMap: {
-    min: 0,
-    max: 35,
-    right: 5,
-    top: 'middle',
-    itemWidth: 10,
-    inRange: {
-      colorHue: [240, 0]
-    }
-  },
-  series: {
-    type: 'heatmap',
-    data: [],
   }
 })
-
-const random = () => {
-  const newdata = []
-  for (let i = 0; i < 300; i++) {
-    for (let j = 0; j < 30; j++) {
-      newdata.push({
-        timestamp: Date.now() - (30 + i) * 100,
-        subcarrier: j,
-        csi: Math.random() * 35
-      })
-    }
-  }
-}
 </script>
 
 <template>
-  <VCharts class="chart" :option="option" autoresize />
-  <button @click="random">random csi</button>
+  <VChart class="chart" :option="option" autoresize />
   <button>collect</button>
 </template>
 
