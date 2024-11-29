107228827f5851a95153e6523d50bcd26b816c61
Xiao Wentao <1626297770@qq.com>
2024-11-29 20:06:34 +0800

    chore: mongoose.connect

--- server/models.ts
+++ server/models.ts
@@ -33,13 +33,3 @@ const userSchema = new Schema<User>({
 const UserModel = model<User>("User", userSchema);
 
 export { UserModel };
-
-// 连接到 MongoDB，如果尚未连接
-if (mongoose.connection.readyState === 0) {
-    mongoose.connect(process.env.MONGODB_URI!, {
-    }).then(() => {
-        console.log('Successfully connected to MongoDB');
-    }).catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
-}
