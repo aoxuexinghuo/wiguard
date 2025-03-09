ab664634b562261976b6cdad9ed55875974d9fd9
Saurlax <saurlax@qq.com>
2025-03-09 10:53:02 +0800

    fix: wrong userId in create device

--- server/api/devices/index.post.ts
+++ server/api/devices/index.post.ts
@@ -3,13 +3,19 @@ import { randomUUID } from "crypto";
 export default defineEventHandler(async (event) => {
   const { name, type, description } = await readBody(event);
   const user = await getUserSession(event);
+  if (!user.user) {
+    throw createError({
+      statusCode: 401,
+      message: "Unauthorized",
+    });
+  }
   return await prisma.device.create({
     data: {
       name,
       description,
       type,
       token: randomUUID(),
-      userId: user.id,
+      userId: user.user?.id,
     },
   });
 });
